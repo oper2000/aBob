6cbd5f5936baab42eab8567491d4d8b2fe51fbb7
liozk <liozk@il.ibm.com>
2016-01-04 16:11:09 +0200

    adding contructor to the new sample test

--- MFBobTests/Android/com.example.amitaim.automationapp/app/AutomationApp/app/src/main/java/com/example/amitaim/automationapp/Tests/AutomaticTest101.java
+++ MFBobTests/Android/com.example.amitaim.automationapp/app/AutomationApp/app/src/main/java/com/example/amitaim/automationapp/Tests/AutomaticTest101.java
@@ -2,6 +2,8 @@ package com.example.amitaim.automationapp.Tests;
 
 import com.example.amitaim.automationapp.MainActivity;
 
+import fi.iki.elonen.NanoHTTPD;
+
 /**
  * Created by amitaim on 07/12/15.
  */
@@ -11,6 +13,11 @@ import com.example.amitaim.automationapp.MainActivity;
 * They MUST update the MainActivity.AutomationServer.result when they finish.
 * */
 public  class AutomaticTest101 extends AutomaticTest {
+
+    public AutomaticTest101(NanoHTTPD.IHTTPSession session){
+
+    }
+    
     public void run() {
         MainActivity.AutomationServer.result = "101";
     }
