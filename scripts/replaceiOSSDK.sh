c4aeb967b57b5d15364d285d371433279ab3f36f
Michael Davidovich <michaeld@michaels-mbp.haifa.ibm.com>
2016-01-19 10:59:52 +0200

    FIX

--- scripts/replaceiOSSDK.sh
+++ scripts/replaceiOSSDK.sh
@@ -5,10 +5,11 @@ SDK_ROOT="/Users/bob/Documents/Developer/Quickbuild/SDK"
 cd "$TEST_ROOT"
 echo "replacing SDK in $TEST_ROOT"
 
-path1=$(find $TEST_ROOT -name IBMMobileFirstPlatformFoundation.framework)
-path2=$(find $TEST_ROOT -name IBMMobileFirstPlatformFoundationOpenSSLUtils.framework)
-path3=$(find $TEST_ROOT -name openssl.framework)
+path1=$(find $TEST_ROOT -name IBMMobileFirstPlatformFoundation.framework | head -1)
+path2=$(find $TEST_ROOT -name IBMMobileFirstPlatformFoundationOpenSSLUtils.framework | head -1)
+path3=$(find $TEST_ROOT -name openssl.framework | head -1)
 
+echo $path1
 
 rm -rf "$path1"
 cp -rf "$SDK_ROOT/imf-client-sdks/IBMMobileFirstPlatformFoundation/Frameworks/IBMMobileFirstPlatformFoundation.framework" "$path1"
@@ -16,3 +17,4 @@ rm -rf "$path2"
 cp -rf "$SDK_ROOT/imf-client-sdks/IBMMobileFirstPlatformFoundation/Frameworks/IBMMobileFirstPlatformFoundationOpenSSLUtils.framework" "$path2"
 rm -rf "$path3"
 cp -rf "$SDK_ROOT/imf-client-sdks/IBMMobileFirstPlatformFoundation/Frameworks/openssl.framework" "$path3"
+
