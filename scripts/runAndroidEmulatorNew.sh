570ad56950823a21298f4e5a9ac624119ae4217f
Lioz Kedmi <Liozk@il.ibm.com>
2016-01-27 16:29:34 +0200

    running android tests in devices

---
+++ scripts/runAndroidEmulatorNew.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+AVD_NAME=$3
+REPORT_NAME=$4
+TEST_SUITE=$5
+deviceURL=$6
+
+
+using_emulator="NO"
+if [[ $AVD_NAME == *"API"* ]]
+then
+echo "using emulator $AVD_NAME"
+device=emulator-5554
+using_emulator="YES"
+else
+echo "using device $AVD_NAME"
+device=$AVD_NAME
+fi
+
+source ~/.bash_profile
+
+waitForEmulator() {
+	echo "start waiting for emulator"
+	seconds=0
+	MAX_WAIT_SECONDS=240
+	res=""
+	while [ $seconds -lt ${MAX_WAIT_SECONDS} ]
+	do
+		sleep 5
+		seconds=`expr $seconds + 5`
+	
+		res=$(adb -s $device shell getprop init.svc.bootanim)
+		if [[ $res == *"stopped"*  ]]
+		then
+			break
+		else
+			echo "Ok. Waiting for Android emulator ${AVD_NAME}. Elapsed time: ${seconds} of ${MAX_WAIT_SECONDS} seconds."
+		fi
+	done
+	if [[ $res = *"stopped"*  ]]
+	then
+		echo "Ok. Android emulator ${AVD_NAME} is ready."
+	else
+		echo "ERROR. Android emulator ${AVD_NAME} is not ready. Waited ${MAX_WAIT_SECONDS} seconds."
+		exit -1
+	fi
+}
+
+
+if [[ $using_emulator = "YES" ]]
+then
+	ps -ef | grep emulator64-x86
+	killall emulator64-x86
+	
+	/Users/liozk/Library/Android/sdk/tools/emulator -avd $AVD_NAME -netspeed full -netdelay none &
+	waitForEmulator
+	echo "back from wait-for-emulator"
+fi
+
+#adb shell getprop init.svc.bootanim
+adb -s $device uninstall $1 
+echo "echo adb -s $device install $2app/build/outputs/apk/app-debug.apk"
+adb -s $device install $2app/build/outputs/apk/app-debug.apk
+
+if [[ $using_emulator = "YES" ]]
+then
+	adb -s $device forward tcp:10081 tcp:10080
+fi
+
+adb -s $device shell am start -n "$1/$1.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+mkdir /Users/bob/Documents/Developer/Quickbuild/Reports/latest/$REPORT_NAME/
+adb -s $device logcat > /Users/bob/Documents/Developer/Quickbuild/Reports/latest/$REPORT_NAME/logcat.log & PID=$!
+
+ant -f /Users/bob/Documents/Developer/Quickbuild/scripts/testng/runTests.xml -Dreport.dir=/Users/bob/Documents/Developer/Quickbuild/Reports/latest/$REPORT_NAME/$1 -DtestFile=$TEST_SUITE -DdeviceUrl=$deviceURL
+ant -f /Users/bob/Documents/Developer/Quickbuild/scripts/testng/runTests.xml replaceTestsName -Dreport.dir=/Users/bob/Documents/Developer/Quickbuild/Reports/latest/$REPORT_NAME/$1
+
+if [[ $using_emulator = "YES" ]]
+then
+	ps -ef | grep emulator64-x86
+	killall emulator64-x86
+else
+kill $PID
+fi
---
+++ scripts/testAndroidNew.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+#emulatorURL=http://127.0.0.1:10081/
+
+
+# Android version YES/NO $1=4 $2=5 $3=6
+
+ if [[ "$1" = "compile" ]]; then
+ yes | rm /Users/bob/Documents/Developer/Quickbuild/Reports/latest/androidEmulatorUsed
+ fi
+
+
+connected_devices=$(adb devices)
+deviceCounter=1
+emulatorCounter=1
+
+if [[ "$1" = "4" ]]; then
+echo "testing using android version 4 is requested"
+if [[ $connected_devices = *"8e8997a0"*  ]]; then
+echo "found device: Galaxy4_4.4.2;8e8997a0 adding it to devices queue"
+DEVICES[$deviceCounter]="Galaxy4_4.4.2;8e8997a0"
+let deviceCounter=$deviceCounter+1
+else
+echo "no device found, adding emulator Nexus_5_API_19 to emulators queue"
+EMULATORS[$emulatorCounter]="Nexus_5_API_19"
+let emulatorCounter=$emulatorCounter+1
+fi
+fi
+
+if [[ "$1" = "5" ]]; then
+echo "testing using android version 5 is requested"
+if [[ $connected_devices = *"TODO: add android 5 device"*  ]]; then
+echo "TODO: found device: ###;### adding it to devices queue"
+DEVICES[$deviceCounter]="TODO: add android 5 device"
+let deviceCounter=$deviceCounter+1
+else
+echo "no device found, adding emulator Nexus_5_API_21 to emulators queue"
+EMULATORS[$emulatorCounter]="Nexus_5_API_21"
+let emulatorCounter=$emulatorCounter+1
+fi
+fi
+
+if [[ "$1" = "6" ]]; then
+echo "testing using android version 6 is requested"
+if [[ $connected_devices = *"038853422152e0b5"*  ]]; then
+echo "found device: Nexus5_6.0;038853422152e0b5 adding it to devices queue"
+DEVICES[$deviceCounter]="Nexus5_6.0;038853422152e0b5"
+let deviceCounter=$deviceCounter+1
+else
+echo "no device found, adding emulator Nexus_5_API_23 to emulators queue"
+EMULATORS[$emulatorCounter]="Nexus_5_API_23"
+let emulatorCounter=$emulatorCounter+1
+fi
+fi
+
+
+cd /Users/bob/Documents/Developer/Quickbuild/tests/Android
+
+for d in */ ; do
+ cd "$d"
+ pwd
+ if [[ "$1" = "compile" ]]; then
+ /Users/bob/Documents/Developer/Quickbuild/scripts/deployAndroidApp.sh ${d%?} 1
+ /Users/bob/Documents/Developer/Quickbuild/scripts/deployAndroidApp.sh ${d%?} 1.0
+ fi
+ if  [ -d "./adapters" ] && [ "$1" = "compile" ] ; then
+ 	cd adapters/
+ 	for a in * ; do
+   		echo "$a\n"
+   		/Users/bob/Documents/Developer/Quickbuild/scripts/deployAdapter.sh $a
+ 	done
+ 	cd ../
+ fi
+ 
+ if [ -f "./setup.sh" ] && [ "$1" = "compile" ] ; then
+	./setup.sh
+ fi
+ 
+ cd app/
+ for d2 in */ ; do
+ if [[ "$1" = "compile" ]]; then
+ yes |cp "$(pwd)/../testSuite.txt" "$(pwd)/${d2}app/src/main/assets/testSuite.txt"
+ /Users/bob/Documents/Developer/Quickbuild/scripts/compileAndroidApp.sh $(pwd)/$d2
+ fi
+ if [[ "$1" != "compile" ]]; then
+ while [ $deviceCounter -gt 1 ]
+ do
+ let deviceCounter=$deviceCounter-1
+ AVD_NAME=$(echo ${DEVICES[$deviceCounter]} | cut -d';' -f2)
+ REPORT_NAME=$(echo ${DEVICES[$deviceCounter]} | cut -d';' -f1)
+ #echo "AVD_NAME $AVD_NAME"
+ deviceIP=$(adb -s $AVD_NAME shell ip addr show | grep 'global wlan0' | cut -d' ' -f6 | cut -d'/' -f1)
+ deviceURL="http://$deviceIP:10080/"
+ #echo "deviceAdd: $deviceURL"
+ /Users/bob/Documents/Developer/Quickbuild/scripts/runAndroidEmulator.sh ${d%?} $(pwd)/$d2 $AVD_NAME $REPORT_NAME $(pwd)/../testSuite.txt $deviceURL
+ done
+ 
+ #ps -ef | grep emulator64-x86
+ #killall emulator64-x86
+ 
+ while [ $emulatorCounter -gt 1 ]
+ do
+  if [ ! -f /Users/bob/Documents/Developer/Quickbuild/Reports/latest/androidEmulatorUsed ]; then
+   echo "" > /Users/bob/Documents/Developer/Quickbuild/Reports/latest/androidEmulatorUsed
+  else
+  echo "emulator is already in use, aborting"
+  exit 1
+  fi
+ let emulatorCounter=$emulatorCounter-1
+ AVD_NAME=${EMULATORS[$emulatorCounter]}
+ REPORT_NAME="Emulator.$AVD_NAME"
+ deviceURL=http://127.0.0.1:10081/
+ /Users/bob/Documents/Developer/Quickbuild/scripts/runAndroidEmulator.sh ${d%?} $(pwd)/$d2 $AVD_NAME $REPORT_NAME $(pwd)/../testSuite.txt $deviceURL
+ done
+
+ #wait
+ fi
+ done
+ cd ../../
+done
