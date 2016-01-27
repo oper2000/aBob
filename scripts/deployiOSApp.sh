e4c54c04d04fe13fa65cc492e33b5aea83a9adeb
Lioz Kedmi <Liozk@il.ibm.com>
2016-01-27 10:38:54 +0200

    changing server auth to admin:admin

--- scripts/deployiOSApp.sh
+++ scripts/deployiOSApp.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR=/Users/bob/Documents/Developer/Quickbuild/scripts
 yes | cp "$SCRIPT_DIR/appDescriptoriOSTemplate.json" "$SCRIPT_DIR/appDescriptoriOS.json"
 sed -i.bak "s/AppBundle/$1/g" "$SCRIPT_DIR/appDescriptoriOS.json"
 sed -i.bak "s/AppVersion/$2/g" "$SCRIPT_DIR/appDescriptoriOS.json"
-curl -X POST -H "Authorization: Basic ZGVtbzpkZW1v" -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "appDescriptoriOS.json=@$SCRIPT_DIR/appDescriptoriOS.json" 'http://localhost:9080/mfpadmin/management-apis/2.0/runtimes/mfp/deploy?type=APP_DESCRIPTOR'
+curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "appDescriptoriOS.json=@$SCRIPT_DIR/appDescriptoriOS.json" 'http://localhost:9080/mfpadmin/management-apis/2.0/runtimes/mfp/deploy?type=APP_DESCRIPTOR'
