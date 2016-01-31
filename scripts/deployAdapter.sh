b3879fb8a9184c1e0279bd0f49d0dbda0b9677b9
Lioz Kedmi <Liozk@il.ibm.com>
2016-01-31 14:20:38 +0200

    changing deploy scripts

--- scripts/deployAdapter.sh
+++ scripts/deployAdapter.sh
@@ -1,9 +1,9 @@
-result=$(curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "$1=@$1" 'http://localhost:9080/mfpadmin/management-apis/2.0/runtimes/mfp/deploy?type=ADAPTER_CONTENT')
+result=$(curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "$1=@$1" 'http://localhost:9080/mfpadmin/management-apis/2.0/runtimes/mfp/adapters')
 echo $result
 if [[ $result == *"FAILURE"* ]]
 then
 sleep 2
-result=$(curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "$1=@$1" 'http://localhost:9080/mfpadmin/management-apis/2.0/runtimes/mfp/deploy?type=ADAPTER_CONTENT')
+result=$(curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "$1=@$1" 'http://localhost:9080/mfpadmin/management-apis/2.0/runtimes/mfp/adapters')
 echo $result
 	if [[ $result == *"FAILURE"* ]]
 	then
