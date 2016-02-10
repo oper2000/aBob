29e88e8bb33f312b720ab408e683036b640d9a80
Eitan Schreiber <SEitan@jmob05.haifa.ibm.com>
2016-02-10 11:02:38 +0200

    set location more global - less bob machine dependant

--- scripts/testHybridApps.sh
+++ scripts/testHybridApps.sh
@@ -17,12 +17,10 @@ cd $SCRIPTS_PATH
 
 ./hybridTestSources/createHybridTestProj.sh $PROJ_NAME
 
-cd $PROJ_NAME
-
 appName=io.cordova.hellocordova
 REPORT_NAME=$TARGET
 
-/Users/bob/Library/Android/sdk/tools/emulator -avd $TARGET -netspeed full -netdelay none  &
+$ANDROID_HOME/tools/emulator -avd $TARGET -netspeed full -netdelay none  &
 
 output=''
 while [[ ${output:0:7} != 'stopped' ]]; do
@@ -33,30 +31,32 @@ done
 echo "emulator is up"
 
 adb -s $device uninstall $appName 
-echo "echo adb -s $device install /Users/bob/Documents/Developer/Quickbuild/scripts/hybridProj/platforms/android/build/outputs/apk/android-x86-debug.apk"
-adb -s $device install /Users/bob/Documents/Developer/Quickbuild/scripts/hybridProj/platforms/android/build/outputs/apk/android-x86-debug.apk
+echo "echo adb -s $device install $SCRIPTS_PATH/hybridProj/platforms/android/build/outputs/apk/android-x86-debug.apk"
+adb -s $device install $SCRIPTS_PATH/hybridProj/platforms/android/build/outputs/apk/android-x86-debug.apk
 
 adb -s $device forward tcp:10081 tcp:10080
 
 adb -s $device shell am start -n "$appName/$appName.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
-mkdir /Users/bob/Documents/Developer/Quickbuild/Reports/latest/$TARGET/
-adb -s $device logcat > /Users/bob/Documents/Developer/Quickbuild/Reports/latest/$TARGET.logcat.log & PID=$!
+mkdir $SCRIPTS_PATH/../Reports/latest/$TARGET/
+adb -s $device logcat > $SCRIPTS_PATH/../Reports/latest/$TARGET.logcat.log & PID=$!
 
 sleep 10
 
-ant -f /Users/bob/Documents/Developer/Quickbuild/scripts/testng/runTests.xml -Dreport.dir=/Users/bob/Documents/Developer/Quickbuild/Reports/latest/$TARGET -DtestFile $SCRIPTS_PATH/hybridTestSources/hybridTestSuite.txt -DdeviceUrl $deviceURL
-ant -f /Users/bob/Documents/Developer/Quickbuild/scripts/testng/runTests.xml replaceTestsName -Dreport.dir=/Users/bob/Documents/Developer/Quickbuild/Reports/latest/$TARGET
+ant -f $SCRIPTS_PATH/testng/runTests.xml -Dreport.dir=$SCRIPTS_PATH/../Reports/latest/$TARGET -DtestFile $SCRIPTS_PATH/hybridTestSources/hybridTestSuite.txt -DdeviceUrl $deviceURL
+ant -f $SCRIPTS_PATH/testng/runTests.xml replaceTestsName -Dreport.dir=$SCRIPTS_PATH/../Reports/latest/$TARGET
 
 # Kill android emulator
 ps -ef | grep emulator64-x86
 killall emulator64-x86
 
+cd $PROJ_NAME
+
 cordova emulate ios
 
 sleep 15
 
-ant -f /Users/bob/Documents/Developer/Quickbuild/scripts/testng/runTests.xml -Dreport.dir=/Users/bob/Documents/Developer/Quickbuild/Reports/latest/$IOS_TARGET -DtestFile $SCRIPTS_PATH/hybridTestSources/hybridTestSuite.txt -DdeviceUrl $ios_deviceURL
-ant -f /Users/bob/Documents/Developer/Quickbuild/scripts/testng/runTests.xml replaceTestsName -Dreport.dir=/Users/bob/Documents/Developer/Quickbuild/Reports/latest/$IOS_TARGET
+ant -f $SCRIPTS_PATH/testng/runTests.xml -Dreport.dir=$SCRIPTS_PATH/../Reports/latest/$IOS_TARGET -DtestFile $SCRIPTS_PATH/hybridTestSources/hybridTestSuite.txt -DdeviceUrl $ios_deviceURL
+ant -f $SCRIPTS_PATH/testng/runTests.xml replaceTestsName -Dreport.dir=$SCRIPTS_PATH/../Reports/latest/$IOS_TARGET
 
 killall "Simulator"
 
