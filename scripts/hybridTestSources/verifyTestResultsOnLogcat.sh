69f494074296f6745216421d97cd93eda306f388
Eitan Schreiber <SEitan@jmob05.haifa.ibm.com>
2016-02-15 13:01:00 +0200

    logcat sanity report

--- scripts/hybridTestSources/verifyTestResultsOnLogcat.sh
+++ scripts/hybridTestSources/verifyTestResultsOnLogcat.sh
@@ -3,11 +3,11 @@ iosSimulatorFolder=`ls -td -- ~/Library/Logs/CoreSimulator/*/ | head -n 1`
 for txt2verify in {"wl.test : debug \[1,2,3\]","wl.test : log testLogger:log message","wl.test : info 1 2 3","wl.test : error Error: testLogger:oh no","wl.test : fatal testLogger: fatal message","MyTestReceiver found. Removing","Showing Splash Screen","testReloadApp","ffff"}
 do
 	found=`grep -c "$txt2verify" ../Reports/latest/$1/logcat.log`  
-	if [ $found == 0 ]; then
+	if [[ $found == 0 ]]; then
 		resultMissing+="Android Platform :$txt2verify<br>"
 	fi
 	found=`grep -c "$txt2verify" $iosSimulatorFolder/system.log`  
-	if [ $found == 0 ]; then
+	if [[ $found == 0 ]]; then
 		resultMissing+="ioS Platform :$txt2verify<br>"
 	fi
 done
