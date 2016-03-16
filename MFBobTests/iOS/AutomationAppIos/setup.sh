051da60737d1d22d23a222decea47e6fdbecfab3
Lioz Kedmi <Liozk@il.ibm.com>
2016-03-16 12:34:40 +0200

    adding ip of Norton's machine

--- MFBobTests/iOS/AutomationAppIos/setup.sh
+++ MFBobTests/iOS/AutomationAppIos/setup.sh
@@ -3,6 +3,6 @@ pod update
 open "AutomationAppIos.xcworkspace"
 sleep 15
 killall Xcode
-if [[ "$USER" = "norton" ]]; then
- 	sed -i.bak s/"<string>ibobs-mac-mini.haifa.ibm.com</string>"/"<string>9.109.250.170</string>"/g $(pwd)/AutomationAppIos/mfpclient.plist
+if [[ "$USER" = "liozk" ]]; then
+ 	sed -i.bak s/"ibobs-mac-mini.haifa.ibm.com"/"9.109.250.170"/g $(pwd)/AutomationAppIos/mfpclient.plist
 fi
