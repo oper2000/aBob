f1ba51e10ffe31f9f56d948d692bd527a75ab6d9
Lioz Kedmi <Liozk@il.ibm.com>
2016-03-28 14:49:40 +0300

    removing ip of norton's mac

--- MFBobTests/iOS/AutomationAppIos/setup.sh
+++ MFBobTests/iOS/AutomationAppIos/setup.sh
@@ -3,6 +3,3 @@ pod update
 open "AutomationAppIos.xcworkspace"
 sleep 15
 killall Xcode
-if [[ "$USER" = "norton" ]]; then
- 	sed -i.bak s/"ibobs-mac-mini.haifa.ibm.com"/"9.109.250.170"/g $(pwd)/AutomationAppIos/mfpclient.plist
-fi
